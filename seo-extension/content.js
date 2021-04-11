29fc81b9ac50427b1503ff01e39548dcec227fda
Mateusz <mateusz.splawski96@gmail.com>
2021-04-11 19:11:20 +0200

    Delete seo-extension directory

--- seo-extension/content.js
+++
@@ -1,4 +0,0 @@
-chrome.runtime.onMessage.addListener((req) => {
-  console.log(req);
-  console.log(document.title);
-});
--- seo-extension/popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("button").addEventListener("click", () => {
-    chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, "message");
-    });
-  });
-});
