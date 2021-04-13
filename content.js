32351bd6731e01372bbe94c0e06ea4758bc995ee
Mateusz <mateusz.splawski96@gmail.com>
2021-04-13 20:32:28 +0200

    Add files via upload

--- content.js
+++ content.js
@@ -1,6 +1,14 @@
 chrome.runtime.onMessage.addListener((req, sender, sendReponse) => {
   const title = document.title;
   const description = document.querySelector("head > meta[name='description']")
-    .content;
-  sendReponse({ title, description });
+    ? document.querySelector("head > meta[name='description']").content
+    : "Empty";
+  const keywords = document.querySelector("head > meta[name='keywords']")
+    ? document.querySelector("head > meta[name='keywords']").content
+    : "Empty";
+  const robots = document.querySelector("head > meta[name='robots']")
+    ? document.querySelector("head > meta[name='robots']").content
+    : "Empty";
+
+  return sendReponse({ title, description, keywords, robots });
 });
--- popup.js
+++ popup.js
@@ -1,17 +1,27 @@
-const handleDOMLoad = () => {
+document.addEventListener("DOMContentLoaded", () => {
   chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, "msg", showMetaData);
   });
-};
+});
 
 const showMetaData = (res) => {
   const titleHtmlElement = document.querySelector("#title");
+  const titleLengthHtmlElement = document.querySelector("#titleLength");
   const descriptionHtmlElement = document.querySelector("#description");
+  const descriptionLengthHtmlElement = document.querySelector(
+    "#descriptionLength"
+  );
+  const keywordsHtmlElement = document.querySelector("#keywords");
+  const keywordsLengthHtmlElement = document.querySelector("#keywordsLength");
+  const robotsHtmlElement = document.querySelector("#robots");
 
-  const { title, description } = res;
+  const { title, description, keywords, robots } = res;
 
   titleHtmlElement.textContent = title;
+  titleLengthHtmlElement.textContent = `${title.length} chars`;
   descriptionHtmlElement.textContent = description;
+  descriptionLengthHtmlElement.textContent = `${description.length} chars`;
+  keywordsHtmlElement.textContent = keywords;
+  keywordsLengthHtmlElement.textContent = `${keywords.length} chars`;
+  robotsHtmlElement.textContent = robots;
 };
-
-document.addEventListener("DOMContentLoaded", handleDOMLoad);
